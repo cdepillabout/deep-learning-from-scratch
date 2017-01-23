14bc64744101a02557b39577334c285478312ce4
(cdep)illabout <cdep.illabout@gmail.com>
2017-01-23 20:55:13 +0900

    Start adding haskell files.

---
+++ haskell/src/Chapter2/Perceptron.hs
@@ -0,0 +1,36 @@
+
+module Chapter2.Perceptron where
+
+import ClassyPrelude hiding (Vector, (<.>))
+
+import Numeric.LinearAlgebra (R, Vector, (<.>), vector)
+
+andPerceptron :: Vector R -> Double
+andPerceptron inputVect =
+  let w = vector [0.5, 0.5]
+      b = -0.7
+      dotProd = inputVect <.> w
+      tmp = dotProd + b
+  in if tmp <= 0 then 0 else 1
+
+nandPerceptron :: Vector R -> Double
+nandPerceptron inputVect =
+  let w = vector [-0.5, -0.5]
+      b = 0.7
+      dotProd = inputVect <.> w
+      tmp = dotProd + b
+  in if tmp <= 0 then 0 else 1
+
+orPerceptron :: Vector R -> Double
+orPerceptron inputVect =
+  let w = vector [0.5, 0.5]
+      b = -0.2
+      dotProd = inputVect <.> w
+      tmp = dotProd + b
+  in if tmp <= 0 then 0 else 1
+
+xorPerceptron :: Vector R -> Double
+xorPerceptron inputVect =
+  let s1 = nandPerceptron inputVect
+      s2 = orPerceptron inputVect
+  in andPerceptron $ vector [s1, s2]
