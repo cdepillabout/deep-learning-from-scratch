0e9f8f630c3d4b7710b2c8aab48c87e530930d1b
(cdep)illabout <cdep.illabout@gmail.com>
2017-01-23 22:04:29 +0900

    Use overloadedlists

--- haskell/src/Chapter2/Perceptron.hs
+++ haskell/src/Chapter2/Perceptron.hs
@@ -3,11 +3,11 @@ module Chapter2.Perceptron where
 
 import ClassyPrelude hiding (Vector, (<.>))
 
-import Numeric.LinearAlgebra (R, Vector, (<.>), vector)
+import Numeric.LinearAlgebra (R, Vector, (<.>))
 
 andPerceptron :: Vector R -> Double
 andPerceptron inputVect =
-  let w = vector [0.5, 0.5]
+  let w = [0.5, 0.5]
       b = -0.7
       dotProd = inputVect <.> w
       tmp = dotProd + b
@@ -15,7 +15,7 @@ andPerceptron inputVect =
 
 nandPerceptron :: Vector R -> Double
 nandPerceptron inputVect =
-  let w = vector [-0.5, -0.5]
+  let w = [-0.5, -0.5]
       b = 0.7
       dotProd = inputVect <.> w
       tmp = dotProd + b
@@ -23,7 +23,7 @@ nandPerceptron inputVect =
 
 orPerceptron :: Vector R -> Double
 orPerceptron inputVect =
-  let w = vector [0.5, 0.5]
+  let w = [0.5, 0.5]
       b = -0.2
       dotProd = inputVect <.> w
       tmp = dotProd + b
@@ -33,4 +33,4 @@ xorPerceptron :: Vector R -> Double
 xorPerceptron inputVect =
   let s1 = nandPerceptron inputVect
       s2 = orPerceptron inputVect
-  in andPerceptron $ vector [s1, s2]
+  in andPerceptron [s1, s2]
