430d3c70722e8f06b941755b81c5e83f8f59c41b
(cdep)illabout <cdep.illabout@gmail.com>
2017-04-11 22:39:49 +0900

    Try implementing this using conduit, but it doesn't work out well.

--- haskell/src/MNIST.hs
+++ haskell/src/MNIST.hs
@@ -1,24 +1,56 @@
 
-module Chapter3.Lib where
+module MNIST where
 
-import Prelude
+import Prelude hiding (dropWhile)
 
-trainImg :: FilePath
-trainImg = "train-images-idx3-ubyte.gz"
+import Conduit
+       (Consumer, Producer, (=$=), ($$), await, concatC, dropC, printC, mapC,
+        sinkVector, sourceFile)
+import Control.Monad (void)
+import Control.Monad.Trans.Resource (MonadResource, runResourceT)
+import Data.ByteString (ByteString)
+import Data.Word (Word8)
+import Numeric.LinearAlgebra (Vector)
+import System.FilePath ((</>))
 
-trainLabel :: FilePath
-trainLabel = "train-labels-idx1-ubyte.gz"
+trainImgPath :: FilePath
+trainImgPath = "mnist" </> "train-images-idx3-ubyte"
 
-testImg :: FilePath
-testImg = "t10k-images-idx3-ubyte.gz"
+trainLabelPath :: FilePath
+trainLabelPath = "mnist" </> "train-labels-idx1-ubyte"
 
-testLabel :: FilePath
-testLabel = "t10k-labels-idx1-ubyte.gz"
+testImgPath :: FilePath
+testImgPath = "mnist" </> "t10k-images-idx3-ubyte"
 
-data Normalize = Normalize | NoNormalize
+testLabelPath :: FilePath
+testLabelPath = "mnist" </> "t10k-labels-idx1-ubyte"
 
 loadMNIST :: IO (Vector Word8, Vector Word8, Vector Word8, Vector Word8)
 loadMNIST = do
-  -- use Conduit.conduitVector??
-  -- or maybe sinkVectorN
+  trainImg <- loadImg trainImgPath
+  testImg <- loadImg testImgPath
+  pure (trainImg, [], testImg, [])
 
+-- TODO: Maybe this would be safer if I used Numeric.LinearAlgebra.fromByteString??
+
+loadImg :: FilePath -> IO (Vector Word8)
+loadImg filePath =
+  runResourceT $
+     source =$= concatC $$ dropC 16 >> sinkVector
+  where
+    source :: MonadResource m => Producer m ByteString
+    source = sourceFile filePath
+
+loadImg' :: FilePath -> IO ()
+loadImg' filePath =
+  void $ runResourceT $
+     -- source =$= concatC $$ dropC 16 >> sinkVector
+     source =$= concatC $$ loop
+  where
+    source :: MonadResource m => Producer m ByteString
+    source = sourceFile filePath
+
+    loop :: Monad m => Consumer a m ()
+    loop = await >>= \case
+      Nothing -> pure ()
+      Just _ -> loop
