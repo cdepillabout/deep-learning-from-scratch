80b3aa23207aca5ee1559e934e4ad670ceba0452
(cdep)illabout <cdep.illabout@gmail.com>
2017-04-17 21:11:06 +0900

    updat

--- haskell/src/MNIST.hs
+++ haskell/src/MNIST.hs
@@ -1,18 +1,17 @@
 
 module MNIST where
 
-import Prelude hiding (drop, dropWhile, readFile)
+import Prelude hiding (drop, dropWhile, map, readFile)
 
-import Conduit
-       (Consumer, Producer, (=$=), ($$), await, concatC, dropC, printC, mapC,
-        sinkVector, sourceFile)
-import Control.Monad (void)
-import Control.Monad.Trans.Resource (MonadResource, runResourceT)
-import Data.ByteString (ByteString, drop, readFile)
+import Data.ByteString (drop, readFile)
+import Data.Vector.Storable (map)
 import Data.Word (Word8)
-import Numeric.LinearAlgebra (Vector)
+import Numeric.LinearAlgebra (Matrix, Vector, cmap, reshape)
 import Numeric.LinearAlgebra.Devel (fromByteString)
 import System.FilePath ((</>))
+
+imgSize :: Num a => a
+imgSize = 784
 
 trainImgPath :: FilePath
 trainImgPath = "mnist" </> "train-images-idx3-ubyte"
@@ -26,17 +25,55 @@ testImgPath = "mnist" </> "t10k-images-idx3-ubyte"
 testLabelPath :: FilePath
 testLabelPath = "mnist" </> "t10k-labels-idx1-ubyte"
 
-loadMNIST :: IO (Vector Word8, Vector Word8, Vector Word8, Vector Word8)
+---------------------
+-- MNIST Data Type --
+---------------------
+
+data MNIST a = MNIST
+  { trainImg :: !a
+  , trainLabel :: !(Vector Word8)
+  , testImg :: !a
+  , testLabel :: !(Vector Word8)
+  }
+
+instance Functor MNIST where
+  fmap :: (a -> b) -> MNIST a -> MNIST b
+  fmap f mnist =
+    MNIST
+    { trainImg = f $ trainImg mnist
+    , trainLabel = trainLabel mnist
+    , testImg = f $ testImg mnist
+    , testLabel = testLabel mnist
+    }
+
+type RawMNIST = MNIST (Matrix Word8)
+
+type NormalMNIST = MNIST (Matrix Double)
+
+normalize :: RawMNIST -> NormalMNIST
+normalize = fmap (undefined {- cmap f -})
+  where
+    f :: Word8 -> Double
+    f byte = fromIntegral byte / 255
+
+-------------------
+-- Loading MNIST --
+-------------------
+
+loadMNIST :: IO RawMNIST
 loadMNIST = do
   trainImg <- loadImg trainImgPath
   trainLabel <- loadLabel trainLabelPath
   testImg <- loadImg testImgPath
   testLabel <- loadLabel testLabelPath
-  pure (trainImg, trainLabel, testImg, testLabel)
+  pure MNIST{..}
 
-loadImg :: FilePath -> IO (Vector Word8)
+loadNormalizedMNIST :: IO NormalMNIST
+loadNormalizedMNIST = normalize <$> loadMNIST
+
+loadImg :: FilePath -> IO (Matrix Word8)
 loadImg filePath =
-  fromByteString . drop 16 <$> readFile filePath
+  reshape imgSize . fromByteString . drop 16 <$> readFile filePath
 
 loadLabel :: FilePath -> IO (Vector Word8)
 loadLabel filePath =
